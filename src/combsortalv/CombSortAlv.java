cb9c67d49eb1dd8d72adcf9eeb3b438191be1598
Xenon <alguirre2@gmil.com>
2023-11-11 19:21:39 +0000

    05 creando metodos de proyecto

--- src/combsortalv/CombSortAlv.java
+++ src/combsortalv/CombSortAlv.java
@@ -9,8 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-
 public class CombSortAlv {
 
 public static void ImprimirArreglo(int[]arreglo){
@@ -23,44 +21,14 @@ public static int Vainter(int inter){
 inter= (inter*10)/13;
 if(inter<1)
     return 1;
-
 return inter;
 }
 
-    
-    public static void main(String[] args) throws IOException {
-// comb sort algorithm by Alvaro aguirre palestina From 4°A  of TIADSM 
-
-
-    Archivos archivo = new Archivos();
-    BufferedReader bufer= new BufferedReader(new InputStreamReader(System.in));
-    String fileName;
-    
-    
-    
-    int[] arr;
-    
-        System.out.println("algoritmo de ordenanmiento comb sort");
-        
-        
-        System.out.println("ingresa el nombre de tu archivo");
-        fileName= bufer.readLine();
-        
-        
-        arr=archivo.fileToInArray(fileName);
-        System.out.println("Imprimimiendo arreglo original");
-        ImprimirArreglo(arr);
-        System.out.println("\n\n");
-        
-        
-        System.out.println("\n ordenando tu arreglo");
-        boolean cambios=true;
+public static int[] combSort(int[] arr){
+    boolean cambios=true;
         int dis=arr.length;
-      
     while(dis!=1 || cambios==true){
-      
         dis=Vainter(dis);
-       
           cambios=false;
             for(int i=0; i<arr.length-dis;i++){
                 if(arr[i]>arr[i+dis]){
@@ -68,25 +36,35 @@ return inter;
             arr[i]=arr[i+dis];
             arr[i+dis]=aux;  
              cambios =true;   
-                    }
-            }
-    }
-
+                    } } }
+return arr;
+}
+    
+    public static void main(String[] args) throws IOException {
+// comb sort algorithm by Alvaro aguirre palestina From 4°A  of TIADSM 
+    Archivos archivo = new Archivos();
+    BufferedReader bufer= new BufferedReader(new InputStreamReader(System.in));
+    String fileName;
+    
+    
+    int[] arr;
+    
+        System.out.println("algoritmo de ordenanmiento comb sort");
+        System.out.println("ingresa el nombre de tu archivo");
+        fileName= bufer.readLine();
+        
+        
+        arr=archivo.fileToInArray(fileName);
+        System.out.println("Imprimimiendo arreglo original");
+        ImprimirArreglo(arr);
+        System.out.println("\n");
+       
+        //ordenando arreglo
+        arr=combSort(arr);
         
         System.out.println("\n imprimiendo tu arreglo arreglado");
-        for (int fix:arr){
-            System.out.println(fix);
-            
-        }
+       ImprimirArreglo(arr);
         System.out.println("tu arreglo es de "+arr.length+" elementos");
-        
-        
-        
-        
-        
-        
-        
-            
 
     }
     
