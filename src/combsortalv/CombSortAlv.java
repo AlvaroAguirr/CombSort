2cbe59070563e01a27ff11cef680f97a1a85fb4e
Xenon <alguirre2@gmil.com>
2023-11-11 17:42:21 +0000

    02 lectura y escritura de arreglo

--- src/combsortalv/CombSortAlv.java
+++ src/combsortalv/CombSortAlv.java
@@ -13,12 +13,39 @@ import java.io.InputStreamReader;
 
 public class CombSortAlv {
 
-
+public static void ImprimirArreglo(int[]arreglo){
+    for(int undato: arreglo){
+        System.out.println(undato);
+    }     
+}
+    
     
     
     public static void main(String[] args) throws IOException {
 // comb sort algorithm by Alvaro aguirre palestina From 4°A  of TIADSM 
 
+
+    Archivos archivo = new Archivos();
+    BufferedReader bufer= new BufferedReader(new InputStreamReader(System.in));
+    String fileName;
+    
+    
+    
+    int[] arr;
+    
+        System.out.println("algoritmo de ordenanmiento comb sort");
+        
+        
+        System.out.println("ingresa el nombre de tu archivo");
+        fileName= bufer.readLine();
+        
+        
+        arr=archivo.fileToInArray(fileName);
+        System.out.println("Imprimimiendo arreglo original");
+        ImprimirArreglo(arr);
+        System.out.println("\n\n");
+        
+        
         
 
     }
