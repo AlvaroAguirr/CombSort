386e42209b70ff72647c1e6c5bab9ffcfa515fe0
Xenon <alguirre2@gmil.com>
2023-11-11 17:38:49 +0000

    01 creacion de proyecto

---
+++ src/combsortalv/CombSortAlv.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package combsortalv;
+
+import Files.Archivos;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class CombSortAlv {
+
+
+    
+    
+    public static void main(String[] args) throws IOException {
+// comb sort algorithm by Alvaro aguirre palestina From 4°A  of TIADSM 
+
+        
+
+    }
+    
+}
